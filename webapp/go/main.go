aa113097567919a4624c3854b8f72bf994ebee01
Siddharth Kannan <mail@siddharthkannan.in>
2020-09-19 19:05:56 +0900

    Should break when == rather than >

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -890,7 +890,7 @@ func searchEstateNazotte(c echo.Context) error {
 			}
 		} else {
 			estatesInPolygon = append(estatesInPolygon, validatedEstate)
-			if len(estatesInPolygon) > NazotteLimit {
+			if len(estatesInPolygon) == NazotteLimit {
 				break
 			}
 		}
